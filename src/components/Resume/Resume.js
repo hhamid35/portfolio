403a1c1b98be0ce278f2a75fad8ff09a54042ed8
Hamid Haris <hhamid35@hotmail.com>
2022-12-18 13:30:14 -0500

    removed spacing

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect} from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
+import Particle from '../Particle/Particle';
 import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import pdf from '../../Assets/HamidHarisResume.pdf';
-import Particle from '../Particle/Particle';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 const resumeLink = 'https://raw.githubusercontent.com/hhamid35/portfolio/main/src/Assets/HamidHarisResume.pdf';
 
@@ -23,7 +24,7 @@ function Resume() {
                 <Row style={{ justifyContent: 'center', position: 'relative' }}>
                     <Button
                         variant='primary'
-                        href={ pdf }
+                        href={pdf}
                         target='_blank'
                         style={{ maxWidth: '250px' }}
                     >
@@ -33,13 +34,13 @@ function Resume() {
                 </Row>
                 <Row className='resume'>
                     <Document file={ resumeLink } className='d-flex justify-content-center'>
-                        <Page pageNumber={ 1 } scale={ width > 786 ? 1.7 : 0.6 } />
+                        <Page wrap={false} pageNumber={1} scale={ width > 786 ? 1.7 : 0.6 } />
                     </Document>
                 </Row>
                 <Row style={{ justifyContent: 'center', position: 'relative' }}>
                     <Button
                         varian='primary'
-                        href={ pdf }
+                        href={pdf}
                         target='_blank'
                         style={{ maxWidth: '250px' }}
                     >
